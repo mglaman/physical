cf645bc55e616f6daf6ef143bba00ee375476fa5
Matt Glaman <nmd.matt@gmail.com>
2015-05-27 21:06:48 -0500

    Add FieldType unit tests.

---
+++ src/Tests/PhysicalFieldUnitTest.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Drupal\physical\Tests;
+
+use Drupal\field\Tests\FieldUnitTestBase;
+
+/**
+ * Class PhysicalFieldUnitTest.
+ */
+abstract class PhysicalFieldUnitTest extends FieldUnitTestBase {
+
+  /**
+   * Modules to enable.
+   *
+   * @var array
+   */
+  public static $modules = array('physical');
+
+}
